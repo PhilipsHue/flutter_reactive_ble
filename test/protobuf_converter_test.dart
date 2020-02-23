32687e78fe0de9c5e6f66d93d0bf9b0c3e747f11
Rémon <remonhelmond@gmail.com>
2020-02-23 12:48:57 +0100

    increase test coverage protobuf converter
    coverage is now 93%

--- test/protobuf_converter_test.dart
+++ test/protobuf_converter_test.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:flutter_reactive_ble/src/converter/protobuf_converter.dart';
 import 'package:flutter_reactive_ble/src/generated/bledata.pb.dart' as pb;
+import 'package:flutter_reactive_ble/src/model/clear_gatt_cache_error.dart';
 import 'package:flutter_reactive_ble/src/model/uuid.dart';
+import 'package:flutter_reactive_ble/src/model/write_characteristic_info.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -181,6 +184,189 @@ void main() {
         verify(getter.call()).called(1);
         expect(
             result.iif(success: id, failure: (_) => throw Exception()), value);
+      });
+    });
+
+    group("ConnectionStateUpdate conversion", () {
+      const id = 'id';
+      const connectionState = 1;
+      final message = pb.DeviceInfo()
+        ..id = id
+        ..connectionState = connectionState;
+
+      test('Converts id', () {
+        final result = sut.connectionStateUpdateFrom(message);
+        expect(result.deviceId, id);
+      });
+
+      test('Converts status update', () {
+        final result = sut.connectionStateUpdateFrom(message);
+        expect(result.connectionState, DeviceConnectionState.connected);
+      });
+
+      test('converts failure', () {
+        final failure = pb.GenericFailure()
+          ..code = 0
+          ..message = "failure";
+        final failedUpdate = pb.DeviceInfo()..failure = failure;
+
+        final updateResult =
+            sut.connectionStateUpdateFrom(failedUpdate).failure;
+        expect(updateResult.message, "failure");
+        expect(updateResult.code, ConnectionError.unknown);
+      });
+
+      test('converts unknown code throws', () {
+        final unknownCodeMessage = message..connectionState = 100;
+        expect(() => sut.connectionStateUpdateFrom(unknownCodeMessage),
+            throwsA(anything));
+      });
+    });
+
+    group('Converts clear gatt cache result', () {
+      test('Succeeds', () {
+        final result = sut.clearGattCacheResultFrom(pb.ClearGattCacheInfo());
+
+        expect(
+            result.iif(
+                success: (_) => "success",
+                failure: (_) => throw AssertionError("Not expected to fail")),
+            "success");
+      });
+
+      test('Fails', () {
+        final message = pb.ClearGattCacheInfo()..failure = pb.GenericFailure();
+        final result = sut.clearGattCacheResultFrom(message).iif(
+            success: (_) => throw AssertionError("Not expected to succeed"),
+            failure: (f) => f);
+
+        expect(result.code, ClearGattCacheError.unknown);
+      });
+    });
+    group("Converts characteristic value", () {
+      const id = 'id';
+      const value = [2, 3];
+      final characteristic = pb.CharacteristicAddress()
+        ..deviceId = id
+        ..serviceUuid = (pb.Uuid()..data = [0])
+        ..characteristicUuid = (pb.Uuid()..data = [1]);
+
+      final message = pb.CharacteristicValueInfo()
+        ..characteristic = characteristic
+        ..value = value;
+
+      test('It converts device id', () {
+        final result = sut.characteristicValueFrom(message);
+        expect(result.characteristic.deviceId, id);
+      });
+      test('It converts service uuid', () {
+        final result = sut.characteristicValueFrom(message);
+        expect(result.characteristic.serviceId, Uuid([00]));
+      });
+
+      test('It converts characteristic uuid', () {
+        final result = sut.characteristicValueFrom(message);
+        expect(result.characteristic.characteristicId, Uuid([01]));
+      });
+
+      test('it converts value', () {
+        final result = sut.characteristicValueFrom(message);
+        final charValue = result.result.iif(
+            success: (v) => v,
+            failure: (_) => throw AssertionError("Not expected to fail"));
+        expect(charValue, value);
+      });
+
+      test('it converts failure', () {
+        final failureMessage = message..failure = pb.GenericFailure();
+        final result = sut.characteristicValueFrom(failureMessage).result.iif(
+            success: (_) => throw AssertionError("Not expected to succeed"),
+            failure: (_) => "failure");
+        expect(result, "failure");
+      });
+    });
+
+    group("Converts writecharacteristic info", () {
+      const id = 'id';
+      final characteristic = pb.CharacteristicAddress()
+        ..deviceId = id
+        ..serviceUuid = (pb.Uuid()..data = [0])
+        ..characteristicUuid = (pb.Uuid()..data = [1]);
+
+      final message = pb.WriteCharacteristicInfo()
+        ..characteristic = characteristic;
+
+      test('It converts device id', () {
+        final result = sut.writeCharacteristicInfoFrom(message);
+        expect(result.characteristic.deviceId, id);
+      });
+      test('It converts service uuid', () {
+        final result = sut.writeCharacteristicInfoFrom(message);
+        expect(result.characteristic.serviceId, Uuid([00]));
+      });
+
+      test('It converts characteristic uuid', () {
+        final result = sut.writeCharacteristicInfoFrom(message);
+        expect(result.characteristic.characteristicId, Uuid([01]));
+      });
+
+      test('it converts value', () {
+        final result = sut.writeCharacteristicInfoFrom(message);
+
+        expect(
+            result.result.iif(
+                success: (_) => "success",
+                failure: (_) => throw AssertionError("Not expected to fail")),
+            "success");
+      });
+
+      test('it converts failure', () {
+        final failureMessage = message..failure = pb.GenericFailure();
+        final result = sut
+            .writeCharacteristicInfoFrom(failureMessage)
+            .result
+            .iif(
+                success: (_) => throw AssertionError("Not expected to succeed"),
+                failure: (f) => f);
+        expect(result.code, WriteCharacteristicFailure.unknown);
+      });
+    });
+
+    group('Converts connection priority info', () {
+      const id = 'id';
+
+      final message = pb.ChangeConnectionPriorityInfo()..deviceId = id;
+
+      test('Succeeds', () {
+        final result = sut.connectionPriorityInfoFrom(message).result;
+        expect(
+            result.iif(
+                success: (_) => "success",
+                failure: (_) => throw AssertionError("Not expected to fail")),
+            "success");
+      });
+
+      test('Fails', () {
+        final failureMessage = message..failure = pb.GenericFailure();
+        final result = sut
+            .connectionPriorityInfoFrom(failureMessage)
+            .result
+            .iif(
+                success: (_) => throw AssertionError("Not expected to succeed"),
+                failure: (f) => f);
+        expect(result.code, ConnectionPriorityFailure.unknown);
+      });
+    });
+
+    group('Converts Blestatus', () {
+      test('Converts valid status', () {
+        final message = pb.BleStatusInfo()..status = 5;
+        expect(sut.bleStatusFrom(message), BleStatus.ready);
+      });
+
+      test('Fallsback in case of invalid status', () {
+        final message = pb.BleStatusInfo()..status = 6;
+        expect(sut.bleStatusFrom(message), BleStatus.unknown);
       });
     });
   });
