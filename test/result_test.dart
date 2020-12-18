71aef9e7093ce3d904f5c0874ec730d8e07eb501
Rémon <remonhelmond@gmail.com>
2020-12-18 18:46:43 +0100

    remove mockito from several tests and replace with stubs

--- test/result_test.dart
+++ test/result_test.dart
@@ -1,40 +1,39 @@
 import 'package:flutter_reactive_ble/src/model/result.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
   group("Result", () {
     test("executes the failure branch for a non-null failure object", () {
       const failureObject = 1;
       const sut = Result<int, int>.failure(failureObject);
-      final handler = _ResultHandlerMock<int, int, int>();
+      final handler = _ResultHandler<int, int, int>();
 
-      sut.iif(success: handler.success, failure: handler.failure);
+      final result =
+          sut.iif(success: handler.success, failure: handler.failure);
 
-      verifyNever(handler.success(any));
-      verify(handler.failure(failureObject)).called(1);
+      expect(result, failureObject);
     });
 
     test("executes the success branch for a non-null success value", () {
       const value = 1;
       const sut = Result<int, int>.success(value);
-      final handler = _ResultHandlerMock<int, int, int>();
+      final handler = _ResultHandler<int, int, int>();
 
-      sut.iif(success: handler.success, failure: handler.failure);
+      final result =
+          sut.iif(success: handler.success, failure: handler.failure);
 
-      verify(handler.success(value)).called(1);
-      verifyNever(handler.failure(any));
+      expect(result, value);
     });
 
     test("executes the success branch for null success value", () {
       const int value = null;
       const sut = Result<int, int>.success(value);
-      final handler = _ResultHandlerMock<int, int, int>();
+      final handler = _ResultHandler<int, int, int>();
 
-      sut.iif(success: handler.success, failure: handler.failure);
+      final result =
+          sut.iif(success: handler.success, failure: handler.failure);
 
-      verify(handler.success(value)).called(1);
-      verifyNever(handler.failure(any));
+      expect(result, null);
     });
 
     test("throws failure as is", () {
@@ -53,10 +52,7 @@ void main() {
   });
 }
 
-class _ResultHandlerMock<Value, Failure, T> extends Mock
-    implements _ResultHandler<Value, Failure, T> {}
-
-abstract class _ResultHandler<Value, Failure, T> {
-  T success(Value value);
-  T failure(Failure failure);
+class _ResultHandler<Value, Failure, T> {
+  T success(Value value) => value as T;
+  T failure(Failure failure) => failure as T;
 }
