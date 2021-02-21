bc19be1753b130c1f7ef32605557c12e64caf6f5
Rémon <remonhelmond@gmail.com>
2021-02-21 21:46:41 +0100

    create expansion panel in order to show discovered services

--- example/lib/src/ui/device_detail/device_interaction_tab.dart
+++ example/lib/src/ui/device_detail/device_interaction_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:flutter_reactive_ble_example/src/ble/ble_device_connector.dart';
+import 'package:flutter_reactive_ble_example/src/ble/ble_device_interactor.dart';
 import 'package:functional_data/functional_data.dart';
 import 'package:provider/provider.dart';
 
@@ -15,15 +16,17 @@ class DeviceInteractionTab extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      Consumer2<BleDeviceConnector, ConnectionStateUpdate?>(
-        builder: (_, deviceConnector, connectionStateUpdate, __) =>
+  Widget build(BuildContext context) => Consumer3<BleDeviceConnector,
+          ConnectionStateUpdate?, BleServiceDiscoverer>(
+        builder: (_, deviceConnector, connectionStateUpdate, serviceDiscoverer,
+                __) =>
             _DeviceInteractionTab(
           viewModel: DeviceInteractionViewModel(
-            deviceId: device.id,
-            connectionStatus: connectionStateUpdate!.connectionState,
-            deviceConnector: deviceConnector,
-          ),
+              deviceId: device.id,
+              connectionStatus: connectionStateUpdate!.connectionState,
+              deviceConnector: deviceConnector,
+              discoverServices: () =>
+                  serviceDiscoverer.discoverServices(device.id)),
         ),
       );
 }
@@ -35,11 +38,14 @@ class DeviceInteractionViewModel extends $DeviceInteractionViewModel {
     required this.deviceId,
     required this.connectionStatus,
     required this.deviceConnector,
+    required this.discoverServices,
   });
 
   final String deviceId;
   final DeviceConnectionState connectionStatus;
   final BleDeviceConnector deviceConnector;
+  @CustomEquality(Ignore())
+  final Future<List<DiscoveredService>> Function() discoverServices;
 
   bool get deviceConnected =>
       connectionStatus == DeviceConnectionState.connected;
@@ -51,13 +57,9 @@ class DeviceInteractionViewModel extends $DeviceInteractionViewModel {
   void disconnect() {
     deviceConnector.disconnect(deviceId);
   }
-
-  void discoverServices() {
-    deviceConnector.discoverServices(deviceId);
-  }
 }
 
-class _DeviceInteractionTab extends StatelessWidget {
+class _DeviceInteractionTab extends StatefulWidget {
   const _DeviceInteractionTab({
     required this.viewModel,
     Key? key,
@@ -66,47 +68,143 @@ class _DeviceInteractionTab extends StatelessWidget {
   final DeviceInteractionViewModel viewModel;
 
   @override
+  __DeviceInteractionTabState createState() => __DeviceInteractionTabState();
+}
+
+class __DeviceInteractionTabState extends State<_DeviceInteractionTab> {
+  late List<DiscoveredService> discoveredServices;
+
+  @override
+  void initState() {
+    discoveredServices = [];
+    super.initState();
+  }
+
+  Future<void> discoverServices() async {
+    final result = await widget.viewModel.discoverServices();
+    setState(() {
+      discoveredServices = result;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
-          child: Text(
-            "ID: ${viewModel.deviceId}",
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        Text(
-          "Status: ${viewModel.connectionStatus}",
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed:
-                    !viewModel.deviceConnected ? viewModel.connect : null,
-                child: const Text("Connect"),
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+                child: Text(
+                  "ID: ${widget.viewModel.deviceId}",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-              ElevatedButton(
-                onPressed:
-                    viewModel.deviceConnected ? viewModel.disconnect : null,
-                child: const Text("Disconnect"),
+              Text(
+                "Status: ${widget.viewModel.connectionStatus}",
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-              ElevatedButton(
-                onPressed: viewModel.deviceConnected
-                    ? viewModel.discoverServices
-                    : null,
-                child: const Text("Discover Services"),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: !widget.viewModel.deviceConnected
+                          ? widget.viewModel.connect
+                          : null,
+                      child: const Text("Connect"),
+                    ),
+                    ElevatedButton(
+                      onPressed: widget.viewModel.deviceConnected
+                          ? widget.viewModel.disconnect
+                          : null,
+                      child: const Text("Disconnect"),
+                    ),
+                    ElevatedButton(
+                      onPressed: widget.viewModel.deviceConnected
+                          ? discoverServices
+                          : null,
+                      child: const Text("Discover Services"),
+                    ),
+                  ],
+                ),
+              ),
+              _ServiceDiscoveryList(
+                discoveredServices: discoveredServices,
               ),
             ],
           ),
         ),
       ],
     );
+  }
+}
+
+class _ServiceDiscoveryList extends StatefulWidget {
+  const _ServiceDiscoveryList({
+    required this.discoveredServices,
+    Key? key,
+  }) : super(key: key);
+
+  final List<DiscoveredService> discoveredServices;
+
+  @override
+  _ServiceDiscoveryListState createState() => _ServiceDiscoveryListState();
+}
+
+class _ServiceDiscoveryListState extends State<_ServiceDiscoveryList> {
+  late final List<int> _expandedItems;
+
+  @override
+  void initState() {
+    _expandedItems = [];
+    super.initState();
+  }
+
+  List<ExpansionPanel> buildPanels() {
+    final panels = <ExpansionPanel>[];
+
+    widget.discoveredServices.asMap().forEach((index, service) => panels.add(
+          ExpansionPanel(
+            body: ListTile(title: Text('${service.characteristicIds.first}')),
+            headerBuilder: (context, isExpanded) => ListTile(
+              title: Text('${service.serviceId}'),
+            ),
+            isExpanded: _expandedItems.contains(index),
+          ),
+        ));
+
+    return panels;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.discoveredServices.isEmpty
+        ? SizedBox()
+        : Padding(
+            padding: const EdgeInsetsDirectional.only(
+              top: 20.0,
+              start: 20.0,
+              end: 20.0,
+            ),
+            child: ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  setState(() {
+                    if (isExpanded) {
+                      _expandedItems.remove(index);
+                    } else {
+                      _expandedItems.add(index);
+                    }
+                  });
+                });
+              },
+              children: [
+                ...buildPanels(),
+              ],
+            ),
+          );
   }
 }
