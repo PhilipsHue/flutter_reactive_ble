484fa8bc044ec0a5ddd76cc09186aea6d29b8183
Remon <remon@hey.com>
2021-09-18 16:16:56 +0200

    rename Task to Subjectask

--- packages/reactive_ble_mobile/ios/Classes/ReactiveBle/Tasks/CharacteristicNotify/CharacteristicNotifyTaskController.swift
+++ packages/reactive_ble_mobile/ios/Classes/ReactiveBle/Tasks/CharacteristicNotify/CharacteristicNotifyTaskController.swift
@@ -4,22 +4,22 @@ struct CharacteristicNotifyTaskController: PeripheralTaskController {
 
     typealias TaskSpec = CharacteristicNotifyTaskSpec
 
-    private let task: Task
+    private let task: SubjectTask
 
-    init(_ task: Task) {
+    init(_ task: SubjectTask) {
         self.task = task
     }
 
-    func start(characteristic: CBCharacteristic) -> Task {
+    func start(characteristic: CBCharacteristic) -> SubjectTask {
         characteristic.service.peripheral.setNotifyValue(task.params.state.isOn, for: characteristic)
         return task.with(state: task.state.processing(.applying))
     }
 
-    func cancel(error: Error) -> Task {
+    func cancel(error: Error) -> SubjectTask {
         return task.with(state: task.state.finished(error))
     }
 
-    func complete(error: Error?) -> Task {
+    func complete(error: Error?) -> SubjectTask {
         return task.with(state: task.state.finished(error))
     }
 }
