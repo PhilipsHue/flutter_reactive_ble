944e2a9cae75e937431dc843693033462694d53f
Sebastian Roth <sebastian.roth@gmail.com>
2021-11-23 08:18:31 +0000

    Improve iOS module compat

--- packages/reactive_ble_mobile/ios/Classes/ReactiveBlePlugin.m
+++ packages/reactive_ble_mobile/ios/Classes/ReactiveBlePlugin.m
@@ -1,5 +1,10 @@
 #import "ReactiveBlePlugin.h"
+
+#if __has_include(<reactive_ble_mobile/reactive_ble_mobile-Swift.h>)
 #import <reactive_ble_mobile/reactive_ble_mobile-Swift.h>
+#else
+#import "reactive_ble_mobile-Swift.h"
+#endif
 
 @implementation ReactiveBlePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
