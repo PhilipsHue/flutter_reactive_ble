2ab73dee1ffe5d306f1d673bf526ac3049b18cd8
Oleksandr Zhuhan <i@ky1vstar.dev>
2021-11-23 13:45:58 +0200

    Declare shim properties as `weak`; add comments with explanations

--- packages/reactive_ble_mobile/ios/Classes/Prelude/CoreBluetooth+Extensions.swift
+++ packages/reactive_ble_mobile/ios/Classes/Prelude/CoreBluetooth+Extensions.swift
@@ -2,17 +2,30 @@ import class CoreBluetooth.CBCharacteristic
 import class CoreBluetooth.CBService
 import class CoreBluetooth.CBPeripheral
 
+/// Extensions below are supposed to backport API breaking changes in CoreBluetooth
+/// framework which Apple introduced with Xcode 13:
+///
+/// ```
+/// + weak var peripheral: CBPeripheral? { get }
+/// - unowned(unsafe) var peripheral: CBPeripheral { get }
+/// ```
+///
+/// Thus this code shadows original property declarations in CoreBluetooth and changes their semantics
+/// from `unsafe non-optional` to `weak optional` to mimic Xcode 13 behaviour.
+///
+/// - Note: This code compiles only when using Xcode 12 and below.
+/// - SeeAlso: https://forums.swift.org/t/is-unowned-unsafe-t-weak-t-a-breaking-change/49917
 #if compiler(<5.5)
 extension CBCharacteristic {
     @nonobjc
-    var service: CBService? {
+    weak var service: CBService? {
         return value(forKey: #function) as? CBService
     }
 }
 
 extension CBService {
     @nonobjc
-    var peripheral: CBPeripheral? {
+    weak var peripheral: CBPeripheral? {
         return value(forKey: #function) as? CBPeripheral
     }
 }
