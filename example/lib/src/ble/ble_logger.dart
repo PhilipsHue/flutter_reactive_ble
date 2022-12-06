893e77b4924bed5a5428e5bcb647ca7d8af38b23
RutgerVromans <rutger.vromans@signify.com>
2022-12-06 18:56:02 +0100

    583 - Add verbose logging
    - Applied file formatting length of 80 characters

--- example/lib/src/ble/ble_logger.dart
+++ example/lib/src/ble/ble_logger.dart
@@ -1,6 +1,12 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:intl/intl.dart';
 
 class BleLogger {
+  BleLogger({
+    required FlutterReactiveBle ble,
+  }) : _ble = ble;
+
+  final FlutterReactiveBle _ble;
   final List<String> _logMessages = [];
   final DateFormat formatter = DateFormat('HH:mm:ss.SSS');
 
@@ -12,4 +18,9 @@ class BleLogger {
   }
 
   void clearLogs() => _logMessages.clear();
+
+  bool get verboseLogging => _ble.logLevel == LogLevel.verbose;
+
+  void toggleVerboseLogging() =>
+      _ble.logLevel = verboseLogging ? LogLevel.none : LogLevel.verbose;
 }
