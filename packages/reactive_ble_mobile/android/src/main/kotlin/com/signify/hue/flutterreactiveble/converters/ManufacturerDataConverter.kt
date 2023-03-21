cf3706522d35f0178332fa2e15248904be5c9358
Agua.L <438517073@qq.com>
2023-03-21 16:07:40 +0800

    Rename the variable

--- packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/converters/ManufacturerDataConverter.kt
+++ packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/converters/ManufacturerDataConverter.kt
@@ -6,16 +6,16 @@ fun extractManufacturerData(manufacturerData: SparseArray<ByteArray>?): ByteArra
     val rawData = mutableListOf<Byte>()
 
     if (manufacturerData != null && manufacturerData.size() > 0) {
-        var index = 0;
+        var pos = 0;
         for(i in 0 until manufacturerData.size()){
             val companyId = manufacturerData.keyAt(i)
             val payload = manufacturerData.get(companyId)
             rawData.add((companyId.toByte()))
             rawData.add(((companyId.shr(Byte.SIZE_BITS)).toByte()))
-            index += 2;
+            pos += 2;
             var list = payload.asList();
-            rawData.addAll(index, list)
-            index += list.size;
+            rawData.addAll(pos, list)
+            pos += list.size;
         }
     }
 
