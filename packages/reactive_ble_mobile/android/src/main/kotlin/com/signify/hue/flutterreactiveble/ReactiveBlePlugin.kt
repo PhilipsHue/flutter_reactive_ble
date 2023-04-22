7d608733dfcb771477222c25161c4fba8b4a992f
David Domkář <david@domkar.dev>
2023-04-22 17:35:07 +0200

    Make new plugin controller for each flutter engine

--- packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ReactiveBlePlugin.kt
+++ packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ReactiveBlePlugin.kt
@@ -1,40 +1,22 @@
 package com.signify.hue.flutterreactiveble
 
-import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.Result
 
 class ReactiveBlePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
+    private val pluginController = PluginController()
+
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        initializePlugin(binding.binaryMessenger, binding.applicationContext, this)
+        val channel = MethodChannel(binding.binaryMessenger, "flutter_reactive_ble_method")
+        channel.setMethodCallHandler(this)
+
+        pluginController.initialize(binding.binaryMessenger, binding.applicationContext)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        deinitializePlugin()
-    }
-
-    companion object {
-        lateinit var pluginController: PluginController
-
-        @JvmStatic
-        private fun initializePlugin(
-            messenger: BinaryMessenger,
-            context: Context,
-            plugin: ReactiveBlePlugin
-        ) {
-            val channel = MethodChannel(messenger, "flutter_reactive_ble_method")
-            channel.setMethodCallHandler(plugin)
-            pluginController = PluginController()
-            pluginController.initialize(messenger, context)
-        }
-
-        @JvmStatic
-        private fun deinitializePlugin() {
-            pluginController.deinitialize()
-        }
+        pluginController.deinitialize()
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
